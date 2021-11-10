f197c42eda19b6bded22ecfb08ddf52b271a2af4
Priya R <priyareddyece97@gmail.com>
2021-11-10 20:08:44 +0530

    first commit

--- src/pages/AudioPlay.js
+++ src/pages/AudioPlay.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import song from "../audio/Ethir-Neechal.mp3";
+import exploreStyle from "../style/explore.module.css";
+import style from "../style/profile.module.css";
 
 export default class Music extends React.Component {
 
@@ -9,9 +13,24 @@ export default class Music extends React.Component {
     render() {
         return (
             <div>
+                <div className={style.profileContainer}>
+                    <div className={exploreStyle.header}>
+                        <div className={`${exploreStyle.head} text-end mb-0`}>
+                            <Link to="/home">
+                                <img src="/Images/arrow.png" alt="" className={exploreStyle.arrow_icon} />
+                            </Link>
+
+                        </div>
+                    </div>
+                    <img src="/Images/girl.jpg" alt="" className={style.profile_image} />
+                    <h4>Priya</h4>
+                    <img src="/Images/girl2.jpg" alt="" className={style.profile_image} />
+                    <h4>Hazee</h4>
+                </div>
                 <audio className="audio-element">
-                    <source src="https://assets.coderrocketfuel.com/pomodoro-times-up.mp3"></source>
+                    <source src={song}></source>
                 </audio>
+
             </div>
         )
     }
